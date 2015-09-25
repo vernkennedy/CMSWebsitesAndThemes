745503e09e70e7525ef1a332c9a7dc784724c14a
vernkennedy <vzidana14@wooster.edu>
2015-09-25 10:13:49 -0700

    2

--- javascripts/CMSWebsitesAndThemes.js
+++ javascripts/CMSWebsitesAndThemes.js
@@ -1,5 +1,5 @@
 var app = angular.module('myApp', []);
 app.controller('customersCtrl', function($scope, $http) {
-  $http.get("https://raw.githubusercontent.com/vernkennedy/PHPProjects/gh-pages/javascripts/PHPProjects.json")
+  $http.get("https://raw.githubusercontent.com/vernkennedy/CMSWebsitesAndThemes/gh-pages/javascripts/CMSStuff.json")
   .success(function (response) {$scope.names = response.records;});
 });
